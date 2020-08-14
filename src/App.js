f59497d8f4f5ba1f43cb8d7516bc04ea09e502f3
Krystian Zun <krystianzun@Krystians-MacBook-Pro.local>
2020-08-14 17:03:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from './Components/Button/Button';
+
+class App extends Component {
+
+  nextQuoteClickHandler() {
+    console.log('hi');
+  }
+
+  render() {
+    return (
+      <div className="App" id="quote-box">
+        <Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler} />
+      </div>
+    );
+  }
+}
+
+export default App;
